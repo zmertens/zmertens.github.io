a80bb2d3ebcd9c079d756dc0f5533b2eaa25af78
zmertens <zmertens@asu.edu>
2017-05-04 19:29:28 -0700

    Add SDL2 support to emscripten build script

--- cpp/Main.cpp
+++ cpp/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <SDL2/SDL.h>
+
 int main(int argc, char** argv)
 {
 	using namespace std;
--- cpp/build_html.sh
+++ cpp/build_html.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-em++ -std=c++11 -O3 Main.cpp -o Main.html
+em++ -std=c++11 -O3 -s USE_SDL=2 Main.cpp -o Main.html
