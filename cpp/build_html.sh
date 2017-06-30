796000888b7393ec16e2b35708c02f47e2ec1985
zmertens <zmertens@asu.edu>
2017-06-29 17:39:30 -0700

    Add SDL drawing test

--- cpp/build_html.sh
+++ cpp/build_html.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-em++ -std=c++11 -O3 -s USE_SDL=2 -s FULL_ES2=1 Main.cpp -o Main.html
+#em++ -std=c++11 -O3 -s USE_SDL=2 -s FULL_ES2=1 Main.cpp -o Main.html
+emcc -s USE_SDL=2 -s FULL_ES2=1 testdraw2.c SDL_test_common.c -o SDL_test_draw2.html
